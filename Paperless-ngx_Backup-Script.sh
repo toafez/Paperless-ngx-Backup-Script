0bb4d13537db48dbfaef5f0ae39a0c99353b86d5
toafez <toafez@gmx.de>
2025-12-18 17:19:57 +0000

    Initial commit

---
+++ Paperless-ngx_Backup-Script.sh
@@ -0,0 +1,187 @@
+#!/bin/bash
+# Filename: Paperless-ngx-Backup-Script.sh - coded in utf-8
+version="1.0-000"
+
+
+#             Backupskript für Paperless-ngx 
+#    Copyright (C) 2025 by tommes (toafez) | MIT License
+
+
+# --------------------------------------------------------------
+# Verbindliche, benutzerspezifische Angaben
+# --------------------------------------------------------------
+
+# Angaben zur Benutzer- und Gruppenzugehörigkeit
+username="BENUTZERNAME"
+groupname="admin"
+
+# Angaben zum lokalen Datensicherungsziel
+backup_dir="/PFAD/ZUM/DATENSICHERUNGSZIEL"
+
+# Angaben zu Paperless-ngx
+paperless_dir="/PFAD/ZUM/PAPERLESS-NGX-VERZEICHNIS"
+paperless_container="Paperless-ngx"
+paperless_service="webserver"
+
+# Angaben zur PostgreSQL-Datenbank
+postgresql_container="Paperless-ngx-PostgreSQL"
+postgresql_service="db"
+postgresql_user="paperless"
+postgresql_db="paperless"
+
+# --------------------------------------------------------------
+# Optionale, benutzerspezifische Angaben
+# --------------------------------------------------------------
+
+# Pfad und Dateiname der Original Paplerless-ngx YAML-Datei (Docker-Compose)
+yamlfile=("${paperless_dir}"/docker-compose.yaml)
+
+# Pfad und Dateiname der Original Paperless-ngx ENV-Datei (Environment-Attribute)
+envfile=("${paperless_dir}"/*.env)
+
+# Pfad und Dateiname der PostgreSQL Dump-Backup-Datei 
+dumpfile=("${backup_dir}"/postgres-dump.sql)
+
+# Pfad und Dateiname der Protokolldatei
+logfile=("${backup_dir}"/backup-protocol.log)
+
+# --------------------------------------------------------------
+# Rock ’n’ Roll...
+# --------------------------------------------------------------
+
+# Skript sofort beenden wenn Fehlerstatus ungleich null ist
+set -e
+
+# Rückgabewert auf den ersten fehlerhaften Befehl innerhalb der Pipeline setzen
+set -o pipefail
+
+# Falls noch nicht vorhanden, Datensicherungsziel erstellen
+[ ! -d "${backup_dir}" ] &&  mkdir -p "${backup_dir}"
+
+if [ -d "${backup_dir}" ]; then
+
+    # Aktuelles Datum
+    datestamp() {
+        date +"%d.%m.%Y"
+    }
+
+    # Aktuelle Uhrzeit
+    timestamp() {
+        date +"%H:%M:%S"
+    }
+
+    # Falls vorhanden, Protokolldatei löschen. Andernfalls eine neue Protokolldatei erstellen.
+    [ -f "${logfile}" ] && rm -f "${logfile}"
+    [ ! -f "${logfile}" ] && touch "${logfile}"
+
+    # Beginn des Protokolls...
+    hr="---------------------------------------------------------------------------------------------------------"
+
+    # Prüfen, ob das verwendete Skript aktuell ist oder ob ein Update auf GitHub verfügbar ist
+    git_version=$(wget --no-check-certificate --timeout=60 --tries=1 -q -O- "https://raw.githubusercontent.com/toafez/Paperless-ngx-Backup-Script/Paperless-ngx-Backup-Script.sh" | grep ^version | cut -d '"' -f2)		
+    if [ -n "${git_version}" ] && [ -n "${version}" ]; then
+        if dpkg --compare-versions ${git_version} gt ${version}; then
+            echo "${hr}" | tee -a "${logfile}" | tee -a "${logfile}"
+            echo "WICHTIGER HINWEIS:" | tee -a "${logfile}"
+            echo "Auf GitHub steht ein Update für dieses Skript zur Verfügung." | tee -a "${logfile}"
+            echo "Bitte aktualisiere deine Version ${version} auf die neue Version ${git_version}." | tee -a "${logfile}"
+            echo "Link: https://github.com/toafez/Paperless-ngx-Backup-Script" | tee -a "${logfile}"
+            echo "${hr}" | tee -a "${logfile}" | tee -a "${logfile}"
+            echo "" | tee -a "${logfile}" | tee -a "${logfile}"
+        fi
+    fi
+
+    echo "${hr}" | tee -a "${logfile}"
+    echo "Paperless-ngx Datensicherungsprotokoll vom $(datestamp) um $(timestamp) Uhr" | tee -a "${logfile}"
+    echo " - Datensicherungsziel: ${backup_dir}" | tee -a "${logfile}"
+    echo "${hr}" | tee -a "${logfile}"
+    echo "" | tee -a "${logfile}"
+
+    if [ -d ${paperless_dir} ]; then
+
+        # Prüfen, ob der Paperless-ngx-Container läuft
+        if [[ $(docker inspect -f '{{.State.Running}}' ${paperless_container} 2>/dev/null) ]]; then
+
+            echo "Die integrierte Exportfunktion von Paperless-ngx wird ausgeführt...." | tee -a "${logfile}"
+
+            # Sichern aller Dokumente in den Paperless-NGX-Exportordner (-d entfernt zwischenzeitlich gelöschte Dateien aus dem Exportordner)
+            docker compose -f "${yamlfile}" exec -T "${paperless_service}" document_exporter ../export -d
+
+            # Prüfen, ob Dokumente im Paperless-NGX-Exportordner vorhanden sind
+            if [ -n "$(ls -A ${paperless_dir}/export)" ]; then
+
+                # Sichern aller exportierten Dokumente aus dem Paperless-NGX-Exportordner ins Datensicherungsziel
+                rsync -a --delete ${paperless_dir}/export ${backup_dir}
+
+                # Prüfen, ob Dokumente im Datensicherungsziel vorhanden sind
+                if [ -d "${backup_dir}/export" ]; then
+                    echo " - Der Paperless-NGX-Exportordner wurde gesichert." | tee -a "${logfile}"
+                else
+                    echo " - Die Sicherung des Paperless-NGX-Exportordners war nicht möglich." | tee -a "${logfile}"
+                fi
+            else
+                echo " - Die Bereitstellung der Dokumente im Paperless-NGX-Exportordner war nicht möglich." | tee -a "${logfile}"
+            fi
+        else
+            echo " - Die Sicherung des Paperless-NGX-Exportordners kann nicht durchgeführt werden, da der  " | tee -a "${logfile}"
+            echo "   entsprechende [ ${paperless_container} ] Container aktuell nicht ausgeführt wird!" | tee -a "${logfile}"
+        fi
+
+        # Prüfen, ob der PostgreSQL-Container läuft
+        if [[ $(docker inspect -f '{{.State.Running}}' ${postgresql_container} 2>/dev/null) ]]; then
+
+            # Sichern der PostgreSQL-Datenbank im Datensicherungsziel
+            docker compose -f "${yamlfile}" exec -T "${postgresql_service}" pg_dump -U "${postgresql_user}" -d "${postgresql_db}" > "${dumpfile}"
+
+            # Prüfen, ob die Sicherung PostgreSQL-Datenbank erfolgreich war
+            if [ -f "${dumpfile}" ] || [ -s "${dumpfile}" ]; then
+                echo " - Die PostgreSQL-Datenbank wurde gesichert." | tee -a "${logfile}"
+            else
+                echo " - Die Sicherung der PostgreSQL-Datenbank war nicht möglich." | tee -a "${logfile}"
+            fi
+        else
+            echo " - Die Sicherung der PostgreSQL-Datenbank kann nicht durchgeführt werden, da der  " | tee -a "${logfile}"
+            echo "   entsprechende [ ${postgresql_container} ] Container aktuell nicht ausgeführt wird!" | tee -a "${logfile}"
+        fi
+
+        # Prüfen, ob es eine YAML-Datei im Paperless-ngx Verzeichnis gibt
+        if [ -f "${yamlfile}" ]; then
+
+            # Sichern der YAML-Datei
+            rsync -a --delete "${yamlfile}" "${backup_dir}/"
+            backup_yamlfile=("${backup_dir}"/*.yaml)
+            
+            # Prüfen, ob die YAML-Datei im Datensicherungsziel angekommen ist
+            if [ -f "${backup_yamlfile}" ] || [ -s "${backup_yamlfile}" ]; then
+                echo " - Die YAML-Datei wurde gesichert." | tee -a "${logfile}"
+            else
+                echo " - Die Sicherung der YAML-Datei konnte nicht durchgeführt werden." | tee -a "${logfile}"
+            fi
+        fi
+
+        # Prüfen, ob es eine ENV-Datei im Paperless-ngx Verzeichnis gibt
+        if [ -f "${envfile}" ]; then
+
+            # Sichern der ENV-Datei
+            rsync -a --delete "${envfile}" "${backup_dir}/"
+            backup_envfile=("${backup_dir}"/*.env)
+            
+            # Prüfen, ob die ENV-Datei im Datensicherungsziel angekommen ist
+            if [ -f "${backup_envfile}" ] || [ -s "${backup_envfile}" ]; then
+                echo " - Die ENV-Datei wurde gesichert." | tee -a "${logfile}"
+            else
+                echo " - Die Sicherung der ENV-Datei konnte nicht durchgeführt werden." | tee -a "${logfile}"
+            fi
+        fi
+
+        # Passe Ordner- und Dateireche im Sicherungsziel an
+        chown -R ${username}:${groupname} ${backup_dir}
+        echo " - Die Ordner- und Dateirechte im Sicherungsziel wurden auf [ ${username}:${groupname} ] gesetzt." | tee -a "${logfile}"
+        echo "" | tee -a "${logfile}"
+        echo "${hr}" | tee -a "${logfile}"
+    else
+        echo " - Das Paperless-ngx Verzeichnis oder die Docker-Compose Datei wurde nicht gefunden." | tee -a "${logfile}"
+        echo "" | tee -a "${logfile}"
+        echo "${hr}" | tee -a "${logfile}"
+    fi
+fi
--- README.md
+++ README.md
@@ -1,2 +1,113 @@
-# Paperless-ngx-Backup-Script
-Export und anschließende Sicherung von Datenbankinhalten, Metadaten, Benutzerprofilen, Einstellungen usw. von Paperless-ngx
+# Paperless-ngx Backup-Script
+
+## Worum geht es?
+Mithilfe des hier vorgestellten Skripts sollen der Export und die anschließende lokale Sicherung von Datenbankinhalten, Metadaten, Benutzerprofilen, Einstellungen usw. von Paperless-ngx erleichtert werden. Hierzu werden Funktionen genutzt, die sowohl Paperless-ngx als auch PostgreSQL selbst anbieten. Mit rsync werden darüber hinaus weitere wichtige Konfigurationsdateien wie die YAML- und die ENV-Datei gesichert. **Das eigentliche Dockerverzeichnis von Paperless-ngx (z.B. /volume1/docker/Pakerless-ngx) wird dabei jedoch nicht gesichert**, da mit den oben genannten Export-Funktionen und rsync-Aufgaben bereits alle relevanten Daten für eine spätere Wiederherstellung erfasst wurden.
+
+#### _Hinweis: Texte in Großbuchstaben, die sich innerhalb oder außerhalb eckiger Klammern befinden, dienen als Platzhalter und müssen durch eigene Angaben ersetzt werden, können aber an einigen Stellen auch nur der Information dienen. Es ist zu beachten, dass die eckigen Klammern Teil des Platzhalters sind und beim Ersetzen durch eigene Angaben ebenfalls entfernt werden müssen._
+
+## So funktioniert das Skript genau
+
+- **Erstellung eines Datensicherungsprotokolls**  
+Zunächst wird im angegebenen Datensicherungsziel ein neues Protokoll erstellt, das im Folgenden mit Informationen zum aktuellen Sicherungsverlauf beschrieben wird. Dabei wird das mitlaufende Protokoll auch in Echtzeit auf der Kommandozeile ausgegeben. 
+
+- **Ausführung der integrierten Exportfunktion von Paperless-ngx**  
+Zum Exportieren vorhandener Datenbankinhalte, Metadaten, Benutzerprofile und -einstellungen etc. bietet Paperless-ngx mit dem „document_exporter” eine eigene Funktion an. Vor der Ausführung dieser Funktion wird zunächst geprüft, ob der Paperless-ngx-Container läuft, da der Export sonst nicht ausgeführt werden kann. Die exportierten Daten werden im Unterverzeichnis `/export` des Paperless-ngx-Verzeichnisses abgelegt. 
+
+- **Sicherung des Exportverzeichnis `/export`**  
+Nach Abschluss des Exports werden die Daten aus dem Ordner `/export` in das lokale Ziel der Datensicherung übertragen.
+
+- **Ausführung der integrierten Exportfunktion (Dump) von PostgreSQL**  
+Zum Exportieren der eigentlichen Datenbankinhalte bietet PostgreSQL mit `pg_dump' eine eigene Funktion an. Dabei werden die zu exportierenden Datenbankinhalte direkt ins lokale Datensicherungsziel übertragen und in einer Datei mit der Dateiendung .sql gespeichert. Vor der Ausführung dieser Funktion wird zunächst geprüft, ob der PostgreSQL Container von Paperless-ngx läuft, da der Export sonst nicht ausgeführt werden kann.
+ 
+- **Sicherung des YAML- bzw. Docker-Compose-Datei**  
+Befindet sich im Hauptverzeichnis von Paperless-ngx eine YAML- bzw. Docker-Compose-Datei, wird diese ins lokale Datensicherungsziel übertragen und als Datei mit der Endung .yaml gespeichert. Befindet sich die Datei an einem anderen Ort, kann im Skript optional der Pfad und der Dateiname angepasst werden. 
+
+- **Sicherung des ENV- bzw. Environment-Datei**  
+Befindet sich im Hauptverzeichnis von Paperless-ngx eine ENV-Datei, wird diese ins lokale Datensicherungsziel übertragen und als Datei mit der Endung .env gespeichert. Befindet sich die Datei an einem anderen Ort, kann im Skript optional der Pfad und der Dateiname angepasst werden. 
+
+- **Anpassen der Ordner- und Dateirechte im Sicherungsziel**  
+Abschließend werden die Ordner- und Dateirechte im Datensicherungsziel noch an die im Skript angegebenen Benutzer- und Gruppenrechte angepasst.
+
+
+## Installationshinweise
+Mit Hilfe des Kommandozeilenprogramms `curl` kann die Shell-Skript-Datei **Paperless-ngx-Backup-Script.sh** einfach über ein Terminalprogramm deiner Wahl heruntergeladen werden. Als Speicherort bietet sich das eigene Benutzer-Home-Verzeichnis an, es kann jedoch auch jedes andere erreichbare Verzeichnis verwendet werden. Wechsle in das von dir gewählte Verzeichnis. Führe dann den folgenden Befehl aus. Damit wird die Skriptdatei in das ausgewählte Verzeichnis heruntergeladen.
+
+**Download der Shell-Skript-Datei Paperless-ngx-Backup-Script.sh**
+
+	curl -L -O https://raw.githubusercontent.com/toafez/Paperless-ngx-Backup-Script/refs/heads/main/scripts/Paperless-ngx-Backup-Script.sh
+	
+
+Führe anschließend im selben Verzeichnis den folgenden Befehl aus, um der Shell-Skript-Datei **Paperless-ngx-Backup-Script.sh** Ausführungsrechte zu erteilen.
+
+	chmod +x Paperless-ngx-Backup-Script.sh
+
+## Skript manuell ausführen
+Die Shell-Skript-Datei `Paperless-ngx-Backup-Script.sh` sollte **immer** mit Root-Berechtigungen (d. h. mit vorangestelltem sudo-Befehl) oder als Root selbst ausgeführt werden ausgeführt werden.
+
+Der Aufruf selbst erfolgt am besten, indem man den absoluten Pfad, d.h. den Verzeichnispfad, in dem sich die Shell-Skript-Datei `Paperless-ngx-Backup-Script.sh` befindet, voranstellt, wobei auch der relative Pfad genügt, wenn man sich selbst im selben Verzeichnis wie das Shell-Skript befindet. 
+
+Aufruf mit dem absoluten Pfad:
+
+	sudo /PFAD/ZUM/SKRIPT/Paperless-ngx-Backup-Script.sh
+
+Aufruf mit dem relativen Pfad:
+
+	sudo ./Paperless-ngx-Backup-Script.sh
+
+
+## Skript automatisiert über einen Cron-Job ausführen
+
+Erstelle einen systemweiten Cron-Job, der später mit Root-Berechtigungen ausgeführt wird. Führe dazu den folgenden Befehl aus:
+
+```
+sudo crontab -e
+```
+	
+Nach dem Aufruf und der eventuellen Aufforderung, einen bevorzugten Editor zum Bearbeiten der crontab auszuwählen – ich empfehle an dieser Stelle den Editor `nano` – wird an geeigneter Stelle, bestenfalls ganz am Ende des Dokuments, folgender Befehl in abgewandelter Form bzw. nach eigenen Anforderungen eingegeben:
+
+Syntax: 
+```
+* * * * * bash /PFAD/ZUM/SKRIPT/DATEINAME.sh
+┬ ┬ ┬ ┬ ┬ ┬
+│ │ │ │ │ └─ SKript/Kommando
+│ │ │ │ └─── Wochentag (0-7, Sonntag ist 0 oder 7)
+│ │ │ └───── Monat (1-12)
+│ │ └─────── Tag (1-31)
+│ └───────── Stunde (0-23)
+└─────────── Minute (0-59)
+
+Um mehrere spezifische Werte für eine Aufgabe festzulegen, können Felder wie Stunden, Minuten, Tage, Monate oder Wochentage durch Kommata getrennt werden.
+```
+
+Beispiel:
+Ausführung des Skripts jeden Montag und Freitag um 6:00 Uhr
+```
+0 6 * * 1,5 bash /PFAD/ZUM/SKRIPT/paperless-backup.sh
+```
+
+## Beispielausgabe
+Nachfolgend ist eine beispielhafte Protokollausgabe auf der Kommandozeile zu sehen, die entsteht, nachdem das Skript ausgeführt wurde. 
+```
+---------------------------------------------------------------------------------------------------------
+Paperless-ngx Datensicherungsprotokoll vom 18.12.2025 um 06:00:00 Uhr
+ - Datensicherungsziel: /volume2/Datensicherung/Paperless-ngx
+---------------------------------------------------------------------------------------------------------
+
+Die integrierte Exportfunktion von Paperless-ngx wird ausgeführt....
+100%|██████████| 2117/2117 [00:02<00:00, 739.84it/s]
+ - Der Paperless-NGX-Exportordner wurde gesichert.
+ - Die PostgreSQL-Datenbank wurde gesichert.
+ - Die YAML-Datei wurde gesichert.
+ - Die Ordner- und Dateirechte im Sicherungsziel wurden auf [ tommes:admin ] gesetzt.
+
+---------------------------------------------------------------------------------------------------------
+```
+
+## Versionsgeschichte
+- Details zur Versionsgeschichte findest du in der Datei [CHANGELOG](CHANGELOG)
+
+## Hilfe und Diskussion
+- Hilfe und Diskussionen gerne über das UGREEN Forum - DACH Community (Link folgt)
+
+## Lizenz
+- MIT License [LICENSE](LICENSE)
